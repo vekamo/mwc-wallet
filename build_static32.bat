abf1040e10bd0b0e1df91209a3279fbc3fe76505
bayk <konstantin.bay@gmail.com>
2021-01-27 18:03:28 -0800

    fixing the build

--- build_static32.bat
+++ build_static32.bat
@@ -4,5 +4,6 @@ set OPENSSL_LIB_DIR=C:\Program Files (x86)\OpenSSL-Win32/lib/
 set OPENSSL_INCLUDE_DIR=C:\Program Files (x86)\OpenSSL-Win32/include
 set OPENSSL_STATIC=yes
 set LIBCLANG_PATH=C:\Program Files (x86)\LLVM\bin\
+set RUSTFLAGS="-Clink-arg=/FORCE:MULTIPLE"
 
 call .ci\win32_cargo.bat +stable-i686-pc-windows-msvc build --release
--- build_static_linux.sh
+++ build_static_linux.sh
@@ -4,6 +4,7 @@ cargo clean
 export OPENSSL_LIB_DIR=/usr/lib/x86_64-linux-gnu/
 export OPENSSL_INCLUDE_DIR=/usr/include/openssl/
 export OPENSSL_STATIC=yes
+export RUSTFLAGS="-Clink-arg=-Wl,--allow-multiple-definition"
 
 cargo build --release
 
