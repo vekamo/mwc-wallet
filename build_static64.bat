0ee707ddec3c90dd8bf5db9313bd7574315aa5fd
bayk <k@k.com>
2021-01-27 23:35:40 -0800

    reverting back linker tweaks

--- build_static64.bat
+++ build_static64.bat
@@ -4,6 +4,5 @@ set OPENSSL_LIB_DIR=C:\Program Files\OpenSSL-Win64\lib\
 set OPENSSL_INCLUDE_DIR=C:\Program Files\OpenSSL-Win64\include
 set OPENSSL_STATIC=yes
 set LIBCLANG_PATH=C:\Program Files\LLVM\bin\libclang.dll
-set RUSTFLAGS=-Clink-arg=/FORCE:MULTIPLE
 
 call .ci\win64_cargo.bat build --release
