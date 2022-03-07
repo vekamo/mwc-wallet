74bfa3d3af76d95c8d6e2528f919912809bec681
Vek <veklao@protonmail.com>
2022-03-07 04:35:01 +0100

    fix ci roaring_arch=x86-64-v2

--- build_static.sh
+++ build_static.sh
@@ -5,4 +5,6 @@ export OPENSSL_LIB_DIR=/usr/local/opt/openssl@1.1/lib/;
 export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/include;
 export OPENSSL_STATIC=yes
 
+ROARING_ARCH=x86-64-v2
+
 cargo build --release
--- build_static_linux.sh
+++ build_static_linux.sh
@@ -5,5 +5,7 @@ export OPENSSL_LIB_DIR=/usr/lib/x86_64-linux-gnu/
 export OPENSSL_INCLUDE_DIR=/usr/include/openssl/
 export OPENSSL_STATIC=yes
 
+ROARING_ARCH=x86-64-v2
+
 cargo build --release
 
